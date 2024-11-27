3885f3a1001e5fde5b7aad0a0956d8db13216626
gin <73906888+gingkapls@users.noreply.github.com>
2024-11-28 02:59:04 +0530

    fix: capitalize - add undefined check

--- capitalize.ts
+++ capitalize.ts
@@ -1,4 +1,4 @@
 const capitalize = (str: string): string =>
-  str.at(0).toUpperCase() + str.slice(1).toLocaleLowerCase();
+  (str.at(0)?.toUpperCase() ?? "") + str.slice(1).toLocaleLowerCase();
 
 export default capitalize;
